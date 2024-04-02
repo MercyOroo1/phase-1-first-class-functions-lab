e1bb8a03dd7f29bba2d1cd14b919860d790777ef
Mercy Oroo <mercy.oroo@student.moringaschool.com>
2024-04-02 11:34:23 +0300

    first class functions lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+// const returnFirstTwoDrivers = function(array){
+//    newArray=[...array]
+//    result = newArray.slice(0,2)
+//    return result
+// }
+// array = ["a","b","c","d"]
+// function returnFirstTwoDrivers(array){
+//     result=array.slice(0,2)
+//     return result
+//  }
+ 
+ const returnFirstTwoDrivers = (drivers)=> {
+   return drivers.slice(0,2)
+ }
+ const  returnLastTwoDrivers= (drivers)=> {
+   return drivers.slice(-2)
+ }
+
+ const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+ 
+ function createFareMultiplier(fare) {
+   return function innerFunction (value) {
+      return value*fare
+   }
+ }
+
+function fareDoubler (fare) {
+   return fare*2
+}
+
+function fareTripler (fare){
+   return fare*3
+}
+
+function selectDifferentDrivers (drivers,operation) {
+   return operation(drivers)
+}
